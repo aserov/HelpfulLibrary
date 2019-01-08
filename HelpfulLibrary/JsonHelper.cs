65de6f3f8edd1696829176a159995f98b73e8bb8
Alex Serov <alex.serov.mail@gmail.com>
2019-01-08 19:58:13 +0300

    Init commit.

---
+++ HelpfulLibrary/JsonHelper.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace HelpfulLibrary
+{
+    public class JsonHelper
+    {
+        public static T ToObject<T>(string json)
+        {
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+
+        public static string ToJson<T>(T model)
+        {
+            return JsonConvert.SerializeObject(model);
+        }
+    }
+}
